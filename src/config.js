c36ab990a2776c579656eb2f8fa1f306721e62ad
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2024-05-06 14:53:47 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // baseUrl:'https://savannah-project-1.onrender.com/api/v1',
-    baseUrl: 'http://127.0.0.1:8000/api/v1',
+    baseUrl:'https://savannah-project-1.onrender.com/api/v1',
+    // baseUrl: 'http://127.0.0.1:8000/api/v1',
 
   };
   
